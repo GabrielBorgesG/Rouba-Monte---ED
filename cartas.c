182701fce721ff244124c4fe7517b6f4950f5fda
Bernardo Kappaun <95720086+Ber-Ka@users.noreply.github.com>
2024-02-09 12:16:34 -0300

    a

--- cartas.c
+++ cartas.c
@@ -1,23 +1,59 @@
 #include "cartas.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 void empilhaCarta(Pilha *monte, Carta *carta) {
     carta->prox = monte->topo;
     monte->topo = carta;
 }
 
-/* void embaralhaCartas(Pilha *baralho) {
-    int totalCartas = 52;
-
-    // Algoritmo de Fisher-Yates
-    for (int i = totalCartas - 1; i > 0; i--) {
-        int posicaoAleatoria = rand() % (i + 1);
-        Carta cartaAux = baralho->topo[i];
-        baralho->topo[i] = baralho->topo[posicaoAleatoria];
-        baralho->topo[posicaoAleatoria] = cartaAux;
+void embaralhaCartas(Pilha *baralho) {
+    Pilha *temp = NULL; // pilha temporária para ajudar no embaralhamento
+    Carta *cartaAtual = NULL; // ponteiro para a carta atual
+    Carta *cartaAleatoria = NULL; // ponteiro para a carta aleatória
+    int tamanho = 0; // variável para armazenar o tamanho do baralho
+    int posicaoAleatoria = 0; // variável para armazenar a posição aleatória
+    
+    // Obtém o tamanho do baralho
+    for (temp = baralho; temp != NULL; temp = temp->topo) {
+        tamanho++;
     }
-}*/
+    
+    // Itera sobre o baralho, sorteando uma posição aleatória para cada carta
+    for (temp = baralho, tamanho--; tamanho > 0; tamanho--) {
+        srand(time(NULL) + temp->topo->valor + tamanho); // atualiza a semente de rand()
+        posicaoAleatoria = rand() % (tamanho + 1); // sorteia uma posição aleatória entre 0 e tamanho
+        
+        // Encontra a carta atual e a carta aleatória
+        for (cartaAtual = temp, tamanho = 0; tamanho < posicaoAleatoria; tamanho++) {
+            cartaAleatoria = cartaAtual;
+            cartaAtual = cartaAtual->topo;
+        }
+        
+        // Troca as cartas atual e aleatória
+        if (cartaAtual != cartaAleatoria) {
+            Carta *proximoDaAleatoria = cartaAleatoria->prox;
+            Carta *proximoDaAtual = cartaAtual->prox;
+            
+            // Conecta a carta aleatória com a próxima carta da carta atual
+            cartaAleatoria->prox = proximoDaAtual;
+            
+            // Conecta a carta atual com a próxima carta da carta aleatória
+            cartaAtual->prox = proximoDaAleatoria;
+            
+            // Desconecta a carta atual do baralho
+            if (temp->topo == cartaAtual) {
+                temp->topo = cartaAtual;
+            }
+            
+            // Desconecta a carta aleatória do baralho
+            if (temp->topo == cartaAleatoria) {
+                temp->topo = cartaAleatoria;
+            }
+        }
+    }
+}
 
 Pilha* criaBaralho() {
     Pilha *baralho = (Pilha *) malloc(sizeof(Pilha));
@@ -34,7 +70,7 @@ Pilha* criaBaralho() {
         }
     }
 
-    //embaralhaCartas(baralho);
+    embaralhaCartas(baralho);
 
     return baralho;
 }

317e148d793f31884eae84799ca6f73aff536bb3
Bernardo Kappaun <95720086+Ber-Ka@users.noreply.github.com>
2024-02-11 11:54:38 -0300

    Delete executor.bat

--- executor.bat
+++
@@ -1,8 +0,0 @@
-gcc -c lista_circ.c
-gcc -c pilha_enc.c
-gcc -c Rouba_monte.c
-gcc -c lista_enc_dupla.c
-
-gcc pilha_enc.o lista_enc_dupla.o lista_circ.o Rouba_monte.o programa.o -o jogo.exe
-
-.\jogo.exe
